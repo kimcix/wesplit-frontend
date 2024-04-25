0e6093e59737d715815d71863876ca030b377c11
zizangF2023 <zizang@andrew.cmu.edu>
2024-04-24 19:19:01 -0700

    Added subbills

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,34 @@
 'use client';
 import React, {useState, useEffect} from 'react';
+import { useRouter } from 'next/navigation';
 import BottomNavBar from '../components/bottomNavigationBar'
 import TopBar from '../components/topBar'
 import SocketClient from '../components/socket';
 import Popup from '../components/popupWindow';
+import { analysisAPIPrefix } from '../components/apiPrefix';
 
 const HomePage = () => {
+    const router = useRouter();
+    const username = localStorage.getItem('username');
+    if (!username) {
+        router.push('/login');
+    }
     const [notification, setNotification] = useState<{ title: string, body: string } | null>(null);
+    const [subBills, setSubBills] = useState<any[]>([]); // JSON objects for SubBills from db
+
+    const getCurrentDate = (): string => {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+      };
+
+    const getBillDate = (billDate: string): string => {
+        const datetime = (new Date(billDate)).toLocaleString();
+        const date = datetime.split(',')[0];
+        return date;
+    }
 
     useEffect(() => {
         // Connect to socket
@@ -20,11 +42,32 @@ const HomePage = () => {
                 }, 3000);
             }
         });
+        // Fetch user sub bills
+        // TODO: Change the user below
+        const subBillsAPIRequestUrl = analysisAPIPrefix + '/date_query?start_date=2020-01-01&end_date=' + getCurrentDate() + '&user=2fa3';
+        const fetchSubBills = async () => {
+            const response = await fetch(subBillsAPIRequestUrl, {
+                method: 'GET',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Failed to retrieve sub bills');
+            }
+            const res = await response.json();
+            const data = JSON.parse(res);
+            setSubBills(data);
+            console.log('subBill data: ', data);
+            console.log('getCurrentDate: ', getCurrentDate())
+        };
+        // API calls
+        fetchSubBills();
         // Any clean-up code can go here
         return () => {
             SocketClient.close();
         };
-    })
+    }, []);
 
     return (
         <div>
@@ -37,29 +80,23 @@ const HomePage = () => {
                 {/* TODO: Replace with real data */}
                 <div className="text-red-700 mt-2 text-3xl font-bold">Owed $230</div>  
                 <div className="font-bold text-lg self-start mt-4 ml-4 mb-2">Recent Transactions</div>
-                <div className="flex flex-row self-start justify-around mt-1 w-full">
-                    {/* TODO: Replace with real data */}
-                    <div className="rounded-lg shadow-lg w-1/3 border">
-                        <div className="px-3 py-2">
-                            <div className="font-bold text-lg mb-1">Evan</div>
-                            <p className="text-gray-500 text-base mb-1">owes you $75</p>
-                            <p className="text-gray-500 text-base">4/7/2024</p>
-                        </div>
-                    </div>
-                    <div className="rounded-lg shadow-lg w-1/3 border">
-                        <div className="px-3 py-2">
-                            <div className="font-bold text-lg mb-1">Evan</div>
-                            <p className="text-gray-500 text-base mb-1">owes you $75</p>
-                            <p className="text-gray-500 text-base">4/7/2024</p>
-                        </div>
-                    </div>
-                    <div className="rounded-lg shadow-lg w-1/3 border">
-                        <div className="px-3 py-2">
-                            <div className="font-bold text-lg mb-1">Evan</div>
-                            <p className="text-gray-500 text-base mb-1">owes you $75</p>
-                            <p className="text-gray-500 text-base">4/7/2024</p>
-                        </div>
-                    </div>
+                <div className="flex flex-row self-start mt-1 w-full">
+                    {/* TODO: Change subBills below to be recent transactions (that include master bills*/}
+                    {subBills.length === 0 ? (
+                        <p className="mt-2 ml-4 text-gray-700 font-bold">No Recent Transactions</p>
+                    ):(
+                        <>
+                            {subBills.map((subBill, index) => (
+                                <div key={index} className="rounded-lg shadow-lg w-1/3 border">
+                                    <div className="px-3 py-2">
+                                        <div className="font-bold text-lg mb-1">{subBill.creator}</div>
+                                        <p className="text-gray-500 text-base mb-1">you owe ${subBill.total}</p>
+                                        <p className="text-gray-500 text-base">{getBillDate(subBill.creation_time['$date'])}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </>
+                    )}
                 </div>
             </div>
 
