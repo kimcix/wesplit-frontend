ba22171ce63057ea93ed095e82629ced8bb422cd
zizangF2023 <zizang@andrew.cmu.edu>
2024-04-24 22:09:52 -0700

    Add master bills

--- src/app/components/apiPrefix.tsx
+++ src/app/components/apiPrefix.tsx
@@ -2,4 +2,5 @@ export const userManagementAPIPrefix = 'http://127.0.0.1:5000';   // Start Laura
 export const notificationAPIPrefix = 'http://127.0.0.1:5004'; // Start Daniel's server at port 5004 by running 'python app.py'
 export const contactAPIPrefix = 'http://127.0.0.1:5002'; // Start Judy's server at port 5002
 export const analysisAPIPrefix = 'http://127.0.0.1:5003'; // Start Alex's server at 5003
+export const billSplittingAPIPrefix = 'http://127.0.0.1:5005'; // Start Lifan's server at 5005
 // ....Add more API prefix here
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,22 +5,21 @@ import BottomNavBar from '../components/bottomNavigationBar'
 import TopBar from '../components/topBar'
 import SocketClient from '../components/socket';
 import Popup from '../components/popupWindow';
-import { analysisAPIPrefix, contactAPIPrefix } from '../components/apiPrefix';
+import { analysisAPIPrefix, contactAPIPrefix, billSplittingAPIPrefix } from '../components/apiPrefix';
 
 
 const HomePage = () => {
     const router = useRouter();
-   
-    const [notification, setNotification] = useState<{ title: string, body: string } | null>(null);
-    const [subBills, setSubBills] = useState<any[]>([]);
-    const [contacts, setContacts] = useState([]);
-    
-
     const token = localStorage.getItem('token');
     const username = localStorage.getItem('username');
     if (!username) {
         router.push('/login');
     }
+    const [notification, setNotification] = useState<{ title: string, body: string } | null>(null);
+    const [contacts, setContacts] = useState([]);
+    const [subBills, setSubBills] = useState<any[]>([]); // JSON objects for SubBills from db
+    const [masterBills, setMasterBills] = useState<any[]>([]); // JSON objects for masterBills from db
+
 
     const getCurrentDate = (): string => {
         const currentDate = new Date();
@@ -48,7 +47,7 @@ const HomePage = () => {
                 }, 3000);
             }
         });
-        // Fetch user sub bills
+        // Fetch user sub bills (bills he/she owes)
         // TODO: Change the user below
         const subBillsAPIRequestUrl = analysisAPIPrefix + '/date_query?start_date=2020-01-01&end_date=' + getCurrentDate() + '&user=2fa3';
         const fetchSubBills = async () => {
@@ -67,9 +66,23 @@ const HomePage = () => {
             console.log('subBill data: ', data);
             console.log('getCurrentDate: ', getCurrentDate())
         };
-        // API calls
-        fetchSubBills();
-
+        // Fetch user master bills (bills others owe he/she)
+        // const masterBillsAPIRequestUrl = billSplittingAPIPrefix + '/splitBill?creator=' + username;
+        const masterBillsAPIRequestUrl = billSplittingAPIPrefix + '/splitBill?creator=leoren';
+        const fetchMasterBills = async () => {
+            const response = await fetch(masterBillsAPIRequestUrl, {
+                method: 'GET',
+                headers: {
+                'Content-Type': 'application/json'
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Failed to retrieve master bills');
+            }
+            const data = await response.json();
+            console.log('masterBill data: ', data);
+            setMasterBills(data);
+        };
         // Fetch user contacts
         const contactsAPIRequestUrl = contactAPIPrefix + `/contacts/all/${username}`;
         const fetchContacts = async () => {
@@ -92,8 +105,10 @@ const HomePage = () => {
             setContacts(data);
             console.log('contacts data: ', data);
         };
+        // API calls
+        fetchSubBills();
+        fetchMasterBills();
         fetchContacts();
-        console.log('contacts: ', contacts);
         // Any clean-up code can go here
         return () => {
             SocketClient.close();
@@ -110,19 +125,38 @@ const HomePage = () => {
                 <div className="mt-20 text-lg font-bold">Total Balance</div>
                 {/* TODO: Replace with real data */}
                 <div className="text-red-700 mt-2 text-3xl font-bold">Owed $230</div>  
-                <div className="font-bold text-lg self-start mt-4 ml-4 mb-2">Recent Transactions</div>
-                <div className="flex flex-row overflow-x-auto self-start mt-1 w-full ml-4">
+                <div className="text-red-700 font-bold text-lg self-start mt-4 ml-4 mb-2">Bills To Pay</div>
+                <div className="flex flex-row self-start mt-1 w-full">
                     {/* TODO: Change subBills below to be recent transactions (that include master bills*/}
                     {subBills.length === 0 ? (
-                        <p className="mt-2 ml-4 text-gray-700 font-bold">No Recent Transactions</p>
+                        <p className="mt-2 ml-4 text-gray-700 font-bold">No Bills to Pay</p>
                     ):(
                         <>
                             {subBills.map((subBill, index) => (
                                 <div key={index} className="rounded-lg shadow-lg w-1/3 border mr-2">
                                     <div className="px-3 py-2">
-                                        <div className="font-bold text-lg mb-1">{subBill.creator}</div>
-                                        <p className="text-gray-500 text-base mb-1">you owe ${subBill.total}</p>
+                                        <div className="font-bold text-lg mb-1">{subBill.masterbill_name}</div>
+                                        <p className="text-gray-500 text-base mb-1">you owe {subBill.creator} ${subBill.total}</p>
                                         <p className="text-gray-500 text-base">{getBillDate(subBill.creation_time['$date'])}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </>
+                    )}
+                </div>
+                <div className="text-green-700 font-bold text-lg self-start mt-6 ml-4 mb-2">Payments To Receive</div>
+                <div className="flex flex-row self-start mt-1 w-full">
+                    {/* TODO: Change subBills below to be recent transactions (that include master bills*/}
+                    {masterBills.length === 0 ? (
+                        <p className="mt-2 ml-4 text-gray-700 font-bold">No Payments to Receive</p>
+                    ):(
+                        <>
+                            {masterBills.map((masterBill, index) => (
+                                <div key={index} className="rounded-lg shadow-lg w-1/3 border">
+                                    <div className="px-3 py-2">
+                                        <div className="font-bold text-lg mb-1">{masterBill.masterBillName}</div>
+                                        <p className="text-gray-500 text-base mb-1">you are owed {masterBill.masterBillTotal}</p>
+                                        {/* <p className="text-gray-500 text-base">{getBillDate(masterBill.creation_time['$date'])}</p> */}
                                     </div>
                                 </div>
                             ))}
@@ -131,8 +165,8 @@ const HomePage = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col items-center mb-8">
-                <div className="font-bold text-lg self-start mt-1 ml-4 mb-2">Your Pinned Contacts</div>
+            <div className="flex flex-col items-center mb-2">
+                <div className="font-bold text-lg self-start ml-4">Your Pinned Contacts</div>
             </div>
 
             <div className="ml-4 mr-4">
@@ -141,15 +175,15 @@ const HomePage = () => {
                     ):(
                         <>
                         {contacts.pinned.individual_contacts.map((contact, index) => (
-                            <div key={contact.id} className="rounded-lg shadow-lg w-full border mb-4 p-4">
-                            <div className="font-bold text-lg mb-1">{contact.name}</div>
-                            <div>{contact.email}</div>
+                            <div key={contact.id} className="rounded-lg shadow-lg w-full border mb-4 p-2">
+                                <div className="font-bold text-lg mb-1">{contact.name}</div>
+                                <div>{contact.email}</div>
                             {/* Include other details as needed */}
                             </div>
                         ))}
                         </>
                     )}
-                </div>
+            </div>
 
             <BottomNavBar />
 
